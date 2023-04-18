e7947c8bc38aaf684e441fb49740dfc8bce2e952
unknown <angelica.basurto@olarena.local>
2023-04-17 22:18:48 -0600

    finishing formulas

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,29 +1,54 @@
 import os
-import csv 
+import csv
 
-budget_data = os.path.join("budget_data.csv")
-text_path = "output.txt" 
-
-#Variables 
-total_months = 0
-total_revenue = 0
-revenue = []
-previous_revenue = 0
-month_of_change = []
-revenue_change = 0
-
-with open('budget_data.csv') as csvfile:  
-    csvreader = csv.DictReader(csvfile)
-
-    for row in csvreader:
-        total_months += 1 
-
-        total_revenue = total_revenue + int(row["1"])
-
-        previous_revenue = float(row["1"])
-        revenue_change = float(row["1"])- previous_revenue
-        month_of_change = [month_of_change] + [row["Date"]]
-        revenue_change_list = revenue_change_list + [revenue_change]
+pollData = os.path.join('PyPoll',"Resources","election_data.csv")
 
 
+with open(pollData, newline="") as csv_file:
+    elections_data = csv.reader(csv_file)
 
+    next(csv.reader)
+    data = list(csv.reader)
+    row_count = len(data)
+
+    candidatelist = list()
+    tally = list()
+    for i in range (0,row_count):
+        candidate = data[i][2]
+        tally.append(candidate)
+        if candidate not in candidatelist: 
+            candidatelist.append(candidate)
+            
+    candidatecount = len(candidatelist)
+    
+    votes = list()
+    percentage = list()
+    for j in range (0,candidatecount):
+        name = candidatelist[j]
+        votes.append(tally.count(name))
+        votesprct = votes[j]/row_count
+        percentage.append(votesprct)
+
+    winner = votes.index(max(votes))     
+
+    print("Election Results")
+    print("----------------------------")
+    print(f"Total Votes: {row_count:,}")
+    print("----------------------------")
+    for k in range (0,candidatecount): 
+        print(f"{candidatelist[k]}: {percentage[k]} ({votes[k]:,})")
+    print("----------------------------")
+    print(f"Winner: {candidatelist[winner]}")
+    
+
+    output = open("output.txt", "w")
+
+
+    # line1 = ("Election Results")
+    # line2 = ("----------------------------")
+    # line3 = (f"Total Votes: {row_count:,}")
+    # line4 = ("----------------------------")
+    # line5 = for k in range (0,candidatecount): 
+    # print(f"{candidatelist[k]}: {percentage[k]} ({votes[k]:,})")
+    # line6 = ("----------------------------")
+    # line7 = (f"Winner: {candidatelist[winner]}")
